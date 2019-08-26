49c37ddd463f47d652c034937701d3c229f054f8
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-08-26 22:00:03 +0100

    implement bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,4 +12,22 @@ def bubble_sort(arr)
     return arr.inspect
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+# puts bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(arr, &block)
+    swaps = false
+    arr.each_with_index do |x, i|
+        unless arr[i+1].nil?
+            if yield(x, arr[i+1]) > 0
+                swaps = true
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+            end
+        end
+    end
+    bubble_sort_by(arr, &block) if swaps
+    puts arr.inspect unless swaps
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+end
