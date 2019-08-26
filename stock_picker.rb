d0a401791484c859aa5a71d812fbe63ad2c4f533
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-08-26 16:40:40 +0100

    Improve assignment of best_days

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,13 +5,11 @@ def stock_picker(arr)
         arr[i..-1].each_with_index do |cmp, j|
             if num < cmp && cmp - num > best_profit
                 best_profit = cmp - num
-                best_days = []
-                best_days << i
-                best_days << j+1
+                best_days = [i,j+1]
             end
         end
     end
-    puts "#{best_days}for a profit of $#{best_profit}"
+    puts "#{best_days} for a profit of $#{best_profit}"
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
